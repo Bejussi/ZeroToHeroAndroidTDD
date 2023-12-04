ae75826ff4f4918d165eb8f63a2a6501165a37dd
Bejussi <pindooqy@gmail.com>
2023-12-04 18:27:13 +0200

    task/011 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,26 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val count = Count.Base(2)
+
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.incrementButton)
+        textView = findViewById(R.id.countTextView)
+
+        button.setOnClickListener {
+            val result = count.increment(textView.text.toString())
+            textView.text = result
+        }
     }
 }
